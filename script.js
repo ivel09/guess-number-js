8edb88bf47fdfc55344a39ce2d6988e50e6a93ac
levi manasis <artasgg09@gmail.com>
2025-06-08 14:45:37 -0500

    primer script interactivo js - clase 09

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let nombre = prompt("¿Cómo te llamas?");
+let edad = Number( prompt("¿Cuántos años tienes?"));
+let ciudad = prompt("¿donde vives?")
+alert(`hola ${nombre} tienes ${edad} años ,vives en ${ciudad}`);
+console.log(`Nombre ingresado:  ${nombre}`);
+console.log(`Edad ingresada: ${edad}`);
+console.log (`ciudad ingresada: ${ciudad}`)
+alert( `${edad <=18} eres mayor` )
